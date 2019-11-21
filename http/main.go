1f996b5c75ae1d10a6c37fe4975a8da23e86b4c9
Selena Solis <selena@fathom5.co>
2019-11-21 12:26:16 -0600

    created a directory for udemy interfaces exercise

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,5 +16,21 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	lw := logWriter{}
+	//has same result as code below
+	//func Copy(dst Writer, src Reader) (written int64, err error)
+	//
+	io.Copy(lw, resp.Body)
+
+	// make an empty byte slice with 99,999 spaces for elements
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// prints out HTML
+	// fmt.Println(string(bs))
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("just wrote this many bytes: ", len(bs))
+	return len(bs), nil
 }

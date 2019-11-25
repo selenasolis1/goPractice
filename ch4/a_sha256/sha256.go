3891d4da95f78aa9df9ea91efc82148f4fd31538
Selena Solis <selena@fathom5.co>
2019-11-25 15:49:41 -0600

    added text read from file to sha256 and nonempty function

--- ch4/a_sha256/sha256.go
+++ ch4/a_sha256/sha256.go
@@ -3,10 +3,25 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
-	c1 := sha256.Sum256([]byte("x"))
-	c2 := sha256.Sum256([]byte("X"))
+	f1, err := os.Open("ab.txt")
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+	defer f1.Close()
+	f2, err := os.Open("ab.txt")
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+	defer f2.Close()
+	buf1, _ := ioutil.ReadAll(f1)
+	buf2, _ := ioutil.ReadAll(f2)
+
+	c1 := sha256.Sum256(buf1)
+	c2 := sha256.Sum256(buf2)
 	fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
 }
--- ch4/b_rev/rev.go
+++ ch4/b_rev/rev.go
@@ -13,7 +13,6 @@ func main() {
 }
 
 func reverse(s []int) []int {
-	//copy slice
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
 	}

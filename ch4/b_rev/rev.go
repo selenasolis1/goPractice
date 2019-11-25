17b510380f9087db62ce027d295650406ec141eb
Selena Solis <selena@fathom5.co>
2019-11-25 14:38:25 -0600

    Reverse function with left and right shift functions

--- ch4/b_rev/rev.go
+++ ch4/b_rev/rev.go
@@ -2,14 +2,36 @@ package main
 
 import "fmt"
 
+var a = []int{0, 1, 2, 3, 4, 5}
+
 func main() {
-	var a = []int{0, 1, 2, 3, 4, 5}
-	reverse(a[:])
-	fmt.Println(a)
+
+	// fmt.Println("reverse: ", reverse(a[:]))
+	fmt.Println("original: ", a)
+	fmt.Println("left 2: ", rotateLeft(a[:], 2))
+	fmt.Println("Right 2: ", rotateRight(a[:], 2))
 }
 
-func reverse(s []int) {
+func reverse(s []int) []int {
+	//copy slice
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
 	}
+	return s
+}
+
+func rotateLeft(s []int, r int) []int {
+	b := append(s[:0:0], s...)
+	reverse(b[:2])
+	reverse(b[2:])
+	reverse(b)
+	return b
+}
+
+func rotateRight(s []int, r int) []int {
+	c := append(s[:0:0], s...)
+	reverse(c)
+	reverse(c[:r])
+	reverse(c[r:])
+	return c
 }

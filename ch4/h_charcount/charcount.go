e3fd2705fc320c647dda70eb9ede4b277686e896
Selena Solis <selena@fathom5.co>
2019-11-26 10:22:36 -0600

    rune count function

---
+++ ch4/h_charcount/charcount.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+//charcount computes counts of unicode characters
+func main() {
+	counts := make(map[rune]int)    //counts of unicode characters
+	var utflen [utf8.UTFMax + 1]int //count of lengths of UTF-8 encodings
+	invalid := 0                    //count of invalid UTF-8 characters
+	f1, err := os.Open("hello.txt")
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+	defer f1.Close()
+
+	in := bufio.NewReader(f1)
+	for {
+		r, n, err := in.ReadRune() //returns rune, nbytes, error
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+	}
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+}

3f41748f52db583b52453d3eeeda263d3ff7253e
Selena Solis <selena@fathom5.co>
2019-11-26 14:26:47 -0600

    html template

--- ch4/m_github/github.go
+++ ch4/m_github/github.go
@@ -39,18 +39,36 @@ type User struct {
 }
 
 //create template variable
-const templ = `{{.TotalCount}} issues:
-{{range .Items}}-------------------------------------------------------------------
-Number: {{.Number}}
-User: {{.User.Login}}
-Title: {{.Title | printf "%.64s"}}
-Age: {{.CreatedAt | daysAgo}} days
-{{end}}`
+// const templ = `{{.TotalCount}} issues:
+// {{range .Items}}-------------------------------------------------------------------
+// Number: {{.Number}}
+// User: {{.User.Login}}
+// Title: {{.Title | printf "%.64s"}}
+// Age: {{.CreatedAt | daysAgo}} days
+// {{end}}`
 
-//
-var report = template.Must(template.New("issuelist").
-	Funcs(template.FuncMap{"daysAgo": daysAgo}). //adds daysAgo to set of functions accesible
-	Parse(templ))
+var issueList = template.Must(template.New("issuelist").Parse(`<h1>{{.TotalCount}} issues</h1>
+<table>
+	<tr style = 'text-align: left'>
+		<th>#</th>
+		<th>State</th>
+		<th>User</th>
+		<th>Title</th>
+	</tr>
+	{{range .Items}}
+	<tr>
+		<td><a href='{{.HTMLURL}}'>{{.Number}}</a></td>
+		<td>{{.State}}</td>
+		<td><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></td>
+		<td><a href='{{.HTMLURL}}'>{{.Title}}</a></td>
+	</tr>
+	{{end}}
+	</table>
+`))
+
+// var report = template.Must(template.New("issuelist").
+// 	Funcs(template.FuncMap{"daysAgo": daysAgo}). //adds daysAgo to set of functions accesible
+// 	Parse(templ))
 
 func main() {
 	//command line args sent to SearchIssues function
@@ -58,7 +76,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := report.Execute(os.Stdout, result); err != nil {
+	if err := issueList.Execute(os.Stdout, result); err != nil {
 		log.Fatal(err)
 	}
 	// //prints total number of issues

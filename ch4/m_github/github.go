422054b301c913699ebf1d55d18a627039723bd5
Selena Solis <selena@fathom5.co>
2019-11-26 12:48:24 -0600

    error saving

--- ch4/m_github/github.go
+++ ch4/m_github/github.go
@@ -39,7 +39,7 @@ func main() {
 	result, err := SearchIssues(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
-	}s
+	}
 	fmt.Printf("%d issues:\n", result.TotalCount)
 	for _, item := range result.Items {
 		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)

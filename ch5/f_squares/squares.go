f46e554c75f763fced78e2658d2934437ac35e1b
Selena Solis <selena@fathom5.co>
2019-12-04 13:01:55 -0600

    annonymous function exercise

---
+++ ch5/f_squares/squares.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+//squares returns a function that returns
+//the next square number each time it is called
+func squares() func() int {
+	var x int
+	return func() int {
+		x++
+		return x * x
+	}
+}
+func main() {
+	f := squares()
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+}

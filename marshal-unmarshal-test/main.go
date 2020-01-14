e9dab224517e42c8d60f6ebcd506990d5b0a7fe8
Selena Solis <selena@fathom5.co>
2020-01-14 11:10:37 -0600

    refactor(main): added more print lines for testing

--- marshal-unmarshal-test/main.go
+++ marshal-unmarshal-test/main.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/FATHOM5/goPractice/marshal-unmarshal-test/specx"
+	"github.com/selenasolis1/goPractice/marshal-unmarshal-test/specx"
 )
 
 func main() {
@@ -12,22 +12,25 @@ func main() {
 		FunctionCode: specx.ReadCoils,
 		Data:         specx.ReadCoilsReq(0x0013, 0x0002),
 	}
+	fmt.Println("pdu is", pdu)
 
 	blob, err := json.Marshal(pdu)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("pdu is: %s\n", string(blob))
+	fmt.Printf("pdu after marshal is: %s\n", string(blob))
 
 	pdu2 := specx.PDU{
 		FunctionCode: specx.WriteMultipleCoils,
 		Data:         specx.WriteMultipleCoilsReq(0x0013, 0x0002, 1, []byte{0x30}),
 	}
+	fmt.Println("pdu2 is", pdu2)
+
 	blob2, err := json.Marshal(pdu2)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("pdu2 is: %s\n", string(blob2))
+	fmt.Printf("pdu2 after marshal is: %s\n", string(blob2))
 
 	var pdu3 = specx.PDU{}
 	err = json.Unmarshal(blob, &pdu3)
